b53dc3acc121e47e553b354b6836da92fb88de6d
kinoue-ent <inoken7778@gmail.com>
2024-08-18 15:06:34 +0900

    Create practice1.py

---
+++ Python Beginner's practice/practice1.py
@@ -0,0 +1,15 @@
+# 変数aには3変数bには-6を格納
+a = 3
+b = -6
+
+# 変数plus,minus,multiply,divideにaとbの足し算、引き算、掛け算、割り算の計算結果を全て格納
+plus = a + b
+minus = a - b
+multiply = a * b
+divide = a / b
+
+# 上記の計算結果を格納した変数を全て表示する
+print(plus)
+print(minus)
+print(multiply)
+print(divide)
